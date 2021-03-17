f8c4061f82006a7c584eec676c5fef1c1740da13
Rick <rick@rtbsoft.com>
2021-03-17 11:26:13 -0400

    fix email validation on account page

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -187,11 +187,11 @@ export default {
         this.showAlert = true;
       }
     },
-    isEmailValid() {
+    isEmailValid(email) {
       // see https://stackoverflow.com/questions/201323/how-to-validate-an-email-address-using-a-regular-expression for the source of this regex
       // eslint-disable-next-line no-control-regex
       const regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-      return regex.test(this.email);
+      return regex.test(email);
     }
   }
 };
